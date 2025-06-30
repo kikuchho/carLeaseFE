bfdfbbc25067a46a52e05d61cdc8657d394ec914
kikuccho <yutotachekawa@gmail.com>
2025-06-30 19:05:55 +0900

    edited homepagebottom

--- frontEnd/src/components/mainpage/HomePageBottom.tsx
+++ frontEnd/src/components/mainpage/HomePageBottom.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import '../../styles/HomePageBottom.css';
 import diagL from '../../assets/diagL.png';
 import diagR from '../../assets/diagR.png';
-
+import pt03_icon1 from '../../assets/pt03_icon1.png';
+import pt03_icon2 from '../../assets/pt03_icon2.png';
+import pt03_icon3 from '../../assets/pt03_icon3.png';
+import pt03_icon4 from '../../assets/pt03_icon4.png';
+import pt03_icon5 from '../../assets/pt03_icon5.png';
 
 const HomePageBottom = () => {
 
@@ -12,6 +16,14 @@ const HomePageBottom = () => {
                 <img src={diagL} alt="L" className="titleimg" />
                 <div className='fifthpoint-title'> まだまだあるKINTOの魅力 </div>
                 <img src={diagR} alt="R" className="titleimg" />
+            </div>
+
+            <div className='grid-container'>
+                <div>  <img src={pt03_icon1} alt="Home Page Image" className='grid-img'/> </div>
+                <div>  <img src={pt03_icon2} alt="Home Page Image" className='grid-img'/> </div>
+                <div>  <img src={pt03_icon3} alt="Home Page Image" className='grid-img'/> </div>
+                <div>  <img src={pt03_icon4} alt="Home Page Image" className='grid-img'/> </div>
+                <div>  <img src={pt03_icon5} alt="Home Page Image" className='grid-img'/> </div>
             </div>
             
             <div className="bottom-image">
